7d3855e2df7f9d28966160194c12c3dcedf7ff6c
fabioandrers11 <121508094+fabioandrers11@users.noreply.github.com>
2022-12-27 16:31:39 +0000

    Add files via upload

---
+++ 1desafio.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+
+driver.get('https://www.pingodoce.pt/')
+
+time.sleep(5)
+
+button = driver.find_element(By.CLASS_NAME, "deny")
+button.click()
+
+time.sleep(2)
+
+button1 = driver.find_element(By.ID, "onetrust-accept-btn-handler")
+button1.click()
+
+time.sleep(2)
+
+SearchInput = driver.find_element(By.NAME, "s")
+SearchInput.send_keys("Receitas de Panquecas")
+SearchInput.submit_button.click()
+
+driver.close()
