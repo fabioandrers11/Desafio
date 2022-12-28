56c42f03f9f161b59840a3174c6345c161e8db9f
fabioandrers11 <121508094+fabioandrers11@users.noreply.github.com>
2022-12-28 12:45:48 +0000

    Add files via upload

--- 1desafio.py
+++ 1desafio.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import re
 
 driver = webdriver.Chrome()
 
@@ -22,4 +23,9 @@ SearchInput = driver.find_element(By.NAME, "s")
 SearchInput.send_keys("Receitas de Panquecas")
 SearchInput.submit_button.click()
 
+matches = re.findall(r'panquecas')
+count = len(matches)
+print(f'A palavra "panquecas" aparece {count} vezes.')
+
 driver.close()
+
